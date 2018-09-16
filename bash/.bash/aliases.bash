3518888c33dad135aeb71c29e003f9224d798b28
martin riedel <mail@mrtnrdl.de>
2018-09-16 21:22:39 +0200

    added aliases

---
+++ bash/.bash/aliases.bash
@@ -0,0 +1,53 @@
+## abreviations
+alias cls="clear"
+
+## git
+alias gc="git commit"
+alias gs="git status"
+alias gd="git diff"
+alias gf="git fetch"
+alias gm="git merge"
+alias gma="git merge --abort"
+alias gr="git rebase"
+alias gp="git push"
+alias gpf="git push --force-with-lease"
+alias gu="git unstage"
+alias gg="git graph"
+alias gl="git log --pretty --oneline --graph"
+alias gA="git add -A"
+alias gri="git rebase -i"
+alias grc="git rebase --continue"
+alias gra="git rebase --abort"
+
+## safety first
+alias v="vim"
+alias vi="vim"
+alias emacs="vim"
+alias nano="vim"
+
+### ls -lisa
+alias l="ls -l"
+alias ll="ls -lisa"
+alias lsd="ls -ld */"
+
+### common typos
+alias cd..="cd .."
+
+## vim-like exit
+alias :wq="exit"
+alias :q="exit"
+
+## misc
+### weather
+alias wetter="curl http://wttr.in/nuernberg"
+alias weather="curl http://wttr.in/nuernberg"
+alias q="exit"
+
+## Locations
+### dotfile
+alias dot="cd /root/.kalidots/"
+
+## VPN
+### HTB
+alias htb="sudo openvpn ~/vpn/htb-eu-free.ovpn"
+
